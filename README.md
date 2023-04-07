dfb68fd31db8fcb7908ff7c605ac947af5273af8
Renata <renata.kajcsa@realtech.com>
2023-04-07 22:47:16 +0300

    fixed bugs, refactoring and added readme

--- README.md
+++ README.md
@@ -1,16 +1,18 @@
 # money_converter_app
 
-A money converter app designed using Flutter for learning puposes
+A money converter app designed using Flutter for learning purposes.
+This app converts EUR to RON.
 
-## Getting Started
+Perks:
+    - form validation
+    - paste proof
+    - digits and decimals only
+    - automatically puts currency (EUR) to input string 
+    - limits input to 16 characters
 
-This project is a starting point for a Flutter application.
-
-A few resources to get you started if this is your first Flutter project:
-
-- [Lab: Write your first Flutter app](https://flutter.dev/docs/get-started/codelab)
-- [Cookbook: Useful Flutter samples](https://flutter.dev/docs/cookbook)
-
-For help getting started with Flutter, view our
-[online documentation](https://flutter.dev/docs), which offers tutorials,
-samples, guidance on mobile development, and a full API reference.
+Screenshots:
+<p float="left">
+  <img src="/screenShot_1.png" width="200" />
+  <img src="/screenShot_2.png.png" width="200" /> 
+  <img src="/screenShot_3.png.png" width="200" />
+</p>
--- lib/view/HomePage.dart
+++ lib/view/HomePage.dart
@@ -12,7 +12,7 @@ class MyHomePage extends StatefulWidget {
 class _MyHomePageState extends State<MyHomePage> {
   final _textFormController = TextEditingController();
   final _formKey = GlobalKey<FormState>();
-  double _inputAmount = 0;
+  double _result = -1;
 
   @override
   void initState() {
@@ -28,7 +28,7 @@ class _MyHomePageState extends State<MyHomePage> {
   // convert the input into RON
   void _convertToEuro() {
     var text = _textFormController.text.replaceAll(" EUR", '');
-    // If input is 0, replace it with a string '0'
+    // If input is 0 or 0.00, give result it with a string '0'
     if (RegExp(r'^0(\.0+)').hasMatch(text)) {
       _textFormController.value = const TextEditingValue(text: '0');
       text = 0 as String;
@@ -36,13 +36,14 @@ class _MyHomePageState extends State<MyHomePage> {
 
     // Validate the form and input, then calculate the amount
     _formKey.currentState!.validate() && _textFormController.text == ''
-        ? _inputAmount = 0
-        : _inputAmount = double.parse(text) * 4.9;
+        ? _result = -1
+        : _result = double.parse(text) * 4.9;
   }
 
   // Validate the input value
   String? validate(value) {
-    if (value == null || value.isEmpty) {
+    if (value == null || value.isEmpty || value == ' EUR') {
+      _result = -1;
       return 'Please enter a number';
     }
 
@@ -58,64 +59,62 @@ class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text(widget.title)),
-      body: Column(
-        crossAxisAlignment: CrossAxisAlignment.center,
-        children: <Widget>[
-          Container(
-              height: 270,
-              width: double.infinity,
-              decoration: const BoxDecoration(
-                  image: DecorationImage(
-                      image: AssetImage(
-                          'assets/images/currencyConverterImage.jpg'),
-                      fit: BoxFit.cover)),
-              child: const SizedBox()),
-          Form(
-              key: _formKey,
-              child: TextFormField(
-                  controller: _textFormController,
-                  validator: (value) {
-                    return validate(value);
-                  },
-                  decoration:
-                      const InputDecoration(labelText: "Enter your number"),
-                  keyboardType: const TextInputType.numberWithOptions(
-                      decimal: true, signed: false),
-                  //  Allow digits, decimal point, and ' RON' at the end of the string),
-                  inputFormatters: [
-                    FilteringTextInputFormatter.allow(
-                        RegExp(r'^\d*\.?\d{0,13}( EUR)?$'))
-                  ],
-                  maxLength: 16,
-                  // actually it is 12, because of ' RON'
-                  onChanged: (text) {
-                    // Manipulate the input text
-                    final updatedText =
-                        text.replaceAll(' EUR', '').trim() + ' EUR';
-                    if (updatedText != _textFormController.text) {
-                      final cursorPosition = updatedText.indexOf(' EUR');
-                      _textFormController.value = _textFormController.value
-                          .copyWith(
-                              text: updatedText,
-                              selection: TextSelection.collapsed(
-                                  offset: cursorPosition));
-                    }
-                  })),
-          TextButton(
-              child: const Text('Calculate',
-                  style: TextStyle(fontSize: 20.0, color: Colors.pink)),
-              onPressed: () {
-                setState(() {});
-                _convertToEuro();
-              }),
-          Text(_inputAmount != 0
-              ? _inputAmount.round() == _inputAmount
-                  ? _inputAmount.toString()
-                  : '${_inputAmount.toStringAsFixed(2)} RON' // Removes the 0's after result
-              : '0 RON')
-        ],
-      ),
-    );
+        appBar: AppBar(title: Text(widget.title)),
+        body: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: <Widget>[
+              Container(
+                  height: 270,
+                  width: double.infinity,
+                  decoration: const BoxDecoration(
+                      image: DecorationImage(
+                          image: AssetImage(
+                              'assets/images/currencyConverterImage.jpg'),
+                          fit: BoxFit.cover)),
+                  child: const SizedBox()),
+              Form(
+                  key: _formKey,
+                  child: TextFormField(
+                      controller: _textFormController,
+                      validator: (value) {
+                        return validate(value);
+                      },
+                      decoration:
+                          const InputDecoration(labelText: "Enter your number"),
+                      keyboardType: const TextInputType.numberWithOptions(
+                          decimal: true, signed: false),
+                      //  Allow digits, decimal point, and ' RON' at the end of the string),
+                      inputFormatters: [
+                        FilteringTextInputFormatter.allow(
+                            RegExp(r'^\d*\.?\d{0,13}( EUR)?$'))
+                      ],
+                      maxLength: 16,
+                      // actually it is 12, because of ' RON'
+                      onChanged: (text) {
+                        // Manipulate the input text
+                        final updatedText =
+                            text.replaceAll(' EUR', '').trim() + ' EUR';
+                        if (updatedText != _textFormController.text) {
+                          final cursorPosition = updatedText.indexOf(' EUR');
+                          _textFormController.value = _textFormController.value
+                              .copyWith(
+                                  text: updatedText,
+                                  selection: TextSelection.collapsed(
+                                      offset: cursorPosition));
+                        }
+                      })),
+              TextButton(
+                  child: const Text('Calculate',
+                      style: TextStyle(fontSize: 20.0, color: Colors.pink)),
+                  onPressed: () {
+                    setState(() {});
+                    _convertToEuro();
+                  }),
+              Text(_result != -1
+                  ? _result.round() == _result
+                      ? _result.round().toString() + ' RON'
+                      : '${_result.toStringAsFixed(2)} RON' // Removes the 0's after result
+                  : '')
+            ]));
   }
 }
